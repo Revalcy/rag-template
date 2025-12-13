8a5a791f1d723eb44d995cdb4e23544979bbe517
Jocelyn <a01285780@itesm.mx>
2025-12-13 14:41:06 -0600

    finished vector store

--- backend.py
+++ backend.py
@@ -4,3 +4,17 @@ from langchain_mongodb import MongoDBAtlasVectorSearch
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.schema import Document
+import streamlit as st
+
+MONGO_URI = st.secrets["MONGO_URI"]
+DB_NAME = "vector_store_database"
+COLLECTION_NAME = "embeddings_stream"
+ATLAS_VECTOR_SEARCH = "vector_index_ghw"
+
+def get_vector_store():
+    client = MongoClient(MONGO_URI)
+    collection = client[DB_NAME][COLLECTION_NAME]
+
+    embeddings = GoogleGenerativeAIEmbeddings(model="model/embeddings-001")
+    vector_store = MongoDBAtlasVectorSearch(collection=collection, embedding=embeddings, index_name=ATLAS_VECTOR_SEARCH)
+    return vector_store
